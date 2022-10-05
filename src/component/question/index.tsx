8729b2d918eebf91bc3dae98c207552fec8374f4
username <81845513+june3011@users.noreply.github.com>
2022-10-06 01:25:52 +0900

    🌧 :: 질문 페이지 기능 구현

--- src/component/question/index.tsx
+++ src/component/question/index.tsx
@@ -1,37 +1,109 @@
-import { DATA_FOR_WOMAN } from "../../shared/question";
+import {
+  DATA_FOR_CHILDREN,
+  DATA_FOR_MAN,
+  DATA_FOR_WOMAN,
+} from "../../shared/question";
 import * as S from "./style";
 import * as I from "../../Asset/index";
+import { theme } from "../../theme";
+import { useEffect, useState } from "react";
+import { queries } from "@testing-library/react";
 
 const Question = () => {
+  const DATA = [DATA_FOR_MAN, DATA_FOR_WOMAN, DATA_FOR_CHILDREN];
+  const [category, setCategory] = useState(0);
+  const [isClicked, setIsClicked] = useState<boolean[]>(
+    new Array(DATA_FOR_MAN.length).fill(false)
+  );
+
+  useEffect(() => {
+    console.log(category);
+    setIsClicked(new Array(DATA[category].length).fill(false));
+  }, [category]);
+  useEffect(() => {
+    console.log(isClicked);
+  }, [isClicked]);
+
   return (
     <S.QuestionPageWRap>
       <S.QuestionTitleWrap>
         <S.QuestionTitle>
-          월경이 궁금한 <span style={{ color: "#DE8282" }}>남성</span>분들을위해
+          월경이 궁금한 <span style={{ color: `${theme.main}` }}>남성</span>
+          분들을위해
         </S.QuestionTitle>
       </S.QuestionTitleWrap>
       <S.QuestionCategoryBox>
         <S.QuestionCategoryWrap>
-          <S.QuestionCategory backgroundColor={"#DE8282"} textColor={"#ffffff"}>
+          <S.QuestionCategory
+            backgroundColor={`${category === 0 ? theme.main : theme.gray300}`}
+            textColor={`${category === 0 ? theme.white : theme.gray500}`}
+            onClick={() => {
+              setCategory(0);
+            }}
+          >
             남성
           </S.QuestionCategory>
-          <S.QuestionCategory backgroundColor={"#F6F6F6"} textColor={"#DEDEDE"}>
+          <S.QuestionCategory
+            backgroundColor={`${category === 1 ? theme.main : theme.gray300}`}
+            textColor={`${category === 1 ? theme.white : theme.gray500}`}
+            onClick={() => {
+              setCategory(1);
+            }}
+          >
             여성
           </S.QuestionCategory>
-          <S.QuestionCategory backgroundColor={"#F6F6F6"} textColor={"#DEDEDE"}>
+          <S.QuestionCategory
+            backgroundColor={`${category === 2 ? theme.main : theme.gray300}`}
+            textColor={`${category === 2 ? theme.white : theme.gray500}`}
+            onClick={() => {
+              setCategory(2);
+            }}
+          >
             학생
           </S.QuestionCategory>
         </S.QuestionCategoryWrap>
       </S.QuestionCategoryBox>
       <S.QuestionBox>
-        {DATA_FOR_WOMAN.map((question) => {
+        {DATA[category].map((question) => {
           return (
-            <S.QuestionWrap backgroundColor={"#F6F6F6"} textColor={"#000000"}>
-              <span>{question.question}</span>
-              <S.QuestionUnderArrow>
-                <I.UnderArrow />
-              </S.QuestionUnderArrow>
-            </S.QuestionWrap>
+            <>
+              <S.QuestionTextWrap
+                size={`${isClicked[question.id]}`}
+                backgroundColor={`${
+                  isClicked[question.id] ? theme.main : theme.gray300
+                }`}
+                textColor={`${
+                  isClicked[question.id] ? theme.white : theme.black
+                }`}
+                key={`${question.id}`}
+              >
+                <S.QuestionWrap>
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <S.QuestionCharWrap>Q. </S.QuestionCharWrap>
+                    <span>{question.question}</span>
+                  </div>
+                  <S.QuestionUnderArrow
+                    onClick={() => {
+                      setIsClicked((prev) =>
+                        prev.map((value: any, idx) =>
+                          idx === question.id ? !value : value
+                        )
+                      );
+                    }}
+                  >
+                    <I.UnderArrow />
+                  </S.QuestionUnderArrow>
+                </S.QuestionWrap>
+                {isClicked[question.id] && (
+                  <S.QueestionWithAnswer>
+                    <S.AnswerCharWrap>
+                      <S.AnswerChar>A. </S.AnswerChar>
+                    </S.AnswerCharWrap>
+                    <span>{question.answer}</span>
+                  </S.QueestionWithAnswer>
+                )}
+              </S.QuestionTextWrap>
+            </>
           );
         })}
       </S.QuestionBox>
