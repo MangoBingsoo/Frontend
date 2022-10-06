3122ef2b402e250e6c78e33d4755466e9144809b
jongjin <jongjin024@gmail.com>
2022-10-06 09:18:09 +0900

    🌧 :: 이름및 날짜 ui 구현

--- src/component/main/index.tsx
+++ src/component/main/index.tsx
@@ -1,5 +1,18 @@
+import * as S from "./style";
+
 const Main: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <S.TextBox>
+        <S.NameText>추혜연님의</S.NameText>
+        <S.DateTextList>
+          <S.Text>예상 생리 시작일이</S.Text>
+          <S.Text className="date">13일</S.Text>
+          <S.Text>남았습니다.</S.Text>
+        </S.DateTextList>
+      </S.TextBox>
+    </>
+  );
 };
 
 export default Main;
--- src/component/main/style.ts
+++ src/component/main/style.ts
@@ -1,3 +1,23 @@
 import styled from "styled-components";
+import { theme } from "../../theme";
 
-export const asds = styled.div``;
+export const TextBox = styled.div`
+  margin-top: 40px;
+  margin-left: 20px;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const NameText = styled.div``;
+
+export const DateTextList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 6px;
+`;
+
+export const Text = styled.li`
+  &.date {
+    color: #de8282;
+  }
+`;
