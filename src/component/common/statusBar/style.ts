295c5c7362176e83fcfcde55d47627c0325540bb
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-10-07 00:27:20 +0900

    🌧 :: status bar 추가

---
+++ src/component/common/statusBar/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const _StatusBarContainer = styled.div`
+  width: 100%;
+  z-index: 2;
+  height: 44px;
+  position: absolute;
+  position: fixed;
+  top: 0;
+  > img {
+    height: 44px;
+  }
+`;
