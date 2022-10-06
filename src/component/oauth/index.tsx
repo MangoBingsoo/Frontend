c1ad8353f9970cfa238288c3d9f1fb4cf81eb63f
jongjin <jongjin024@gmail.com>
2022-10-06 16:03:25 +0900

    🌧 :: navigation -> replace

--- src/component/oauth/index.tsx
+++ src/component/oauth/index.tsx
@@ -1,18 +1,14 @@
 // import * as S from "./style";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import AuthApi from "../../api/Auth/Auth.api";
 import { theme } from "../../theme";
 import * as S from "./style";
 
 const Oauth = () => {
-  const navigate = useNavigate();
-
   const move = (res: any) => {
     AuthApi.loginSuccess(res);
-    navigate("/main");
+    window.location.replace("/main");
   };
   return (
     <S.AuthFormWrap backgroundColor={theme.main}>
