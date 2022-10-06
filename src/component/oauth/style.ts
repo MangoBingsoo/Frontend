d253ebe6cf268f1803f1994f5f9bbfaff21e8b53
username <81845513+june3011@users.noreply.github.com>
2022-10-06 17:09:42 +0900

    🌧 :: 구글 oauth 로그인 버튼 수정

--- src/component/oauth/style.ts
+++ src/component/oauth/style.ts
@@ -1,15 +1,48 @@
 import styled from "styled-components";
 
-export const AuthFormWrap = styled.div<{ backgroundColor: string }>`
+export const AuthForm = styled.div<{ backgroundColor: string }>`
   width: 414px;
   height: 896px;
   display: flex;
+  flex-direction: row;
   justify-content: center;
-  align-items: flex-end;
-  padding-bottom: 150px;
+  align-items: center;
   background-color: ${({ backgroundColor }) => backgroundColor};
 `;
 
-export const AuthLoginBtnWrap = styled.div`
+export const AuthFormWrap = styled.div`
+  margin-top: 750px;
+  position: absolute;
+  z-index: 99;
+  opacity: 0;
+`;
+
+export const AuthLoginBtn = styled.div<{
+  backgroundColor: string;
+  textColor: string;
+}>`
+  width: 374px;
+  height: 54px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ textColor }) => textColor};
   display: flex;
+  align-items: center;
+  margin-top: 750px;
+  border-radius: 30px;
+`;
+
+export const AuthLoginImg = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-left: 100px;
+`;
+
+export const AuthLoginText = styled.span`
+  width: 150px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  font-weight: 700;
+  font-size: 18px;
 `;
