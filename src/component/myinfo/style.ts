336c55af990421864b7125d7ee9af7f48d91ab75
username <81845513+june3011@users.noreply.github.com>
2022-10-07 01:56:19 +0900

    🌧 :: 내정보 페이지 - 공지사항, 로그아웃 추가

--- src/component/myinfo/style.ts
+++ src/component/myinfo/style.ts
@@ -26,3 +26,85 @@ export const _IdText = styled.p`
   color: ${({ theme }) => theme.gray300};
   font-size: 16px;
 `;
+
+export const WarmTextWrap = styled.div`
+  width: 414px;
+  height: 38px;
+  position: absolute;
+  top: 258px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const WrapText = styled.span<{
+  backgroundColor: string;
+  textColor: string;
+}>`
+  width: 374px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ textColor }) => textColor};
+`;
+
+export const FunctionWrap = styled.div`
+  width: 414px;
+  height: 63px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  position: absolute;
+  top: 324px;
+`;
+
+export const SecondFunctionWrap = styled.div`
+  width: 414px;
+  height: 63px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  position: absolute;
+  top: 411px;
+`;
+
+export const FunctionLeftWrap = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FunctionRightWrap = styled.div`
+  width: 20%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SubText = styled.span`
+  height: 40%;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  font-size: 13px;
+  color: gray;
+`;
+
+export const MainText = styled.span`
+  height: 60%;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const ArrowImg = styled.img`
+  width: 30px;
+  height: 30px;
+  transform: rotate(180deg);
+`;
